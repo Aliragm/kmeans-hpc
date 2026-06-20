4cec5b692d02d7882dffaa959c16780c05afabf0
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2026-06-20 16:44:41 -0300

    chore: :hammer: add slurm script for mpi openmp kmeans

---
+++ scripts/mpi_openmp_kmeans.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_openmp_kmeans
+#SBATCH --output=mpi_openmp_kmeans_%j.out
+#SBATCH --error=mpi_openmp_kmeans_%j.err
+#SBATCH --partition=amd-512
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --time=0-0:10
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# <k>: clusters
+# <arquivo_dataset>: dataset CSV
+# <start_row>: linha inicial para leitura do dataset (0-indexed)
+# <start_col>: coluna inicial para leitura do dataset (0-indexed)
+# <end_col>: coluna final para leitura do dataset (0-indexed, inclusive)
+mpirun ./mpi_openmp_kmeans 3 Iris.csv 1 1 4
